b0f4bc861a7a30563c35307b11e7e7d55712662d
cdmontez <93397953+cdmontez@users.noreply.github.com>
2021-11-03 15:07:12 -0700

    Update themeSelector.js

--- zdesign/js/themeSelector.js
+++ zdesign/js/themeSelector.js
@@ -1,8 +1,8 @@
-var currentTheme = document.querySelector("#theme");
-function changeTheme () {
-  if (currentTheme.getAttribute("href") == "/zstyle/lightTheme.css") {
-    currentTheme.href = "/zstyle/darkTheme.css";
+const currentTheme = document.querySelector('#theme');
+function changeTheme() {
+  if (currentTheme.getAttribute('href') == '/zstyle/lightTheme.css') {
+    currentTheme.href = '/zstyle/darkTheme.css';
   } else {
-    currentTheme.href = "/zstyle/lightTheme.css";
+    currentTheme.href = '/zstyle/lightTheme.css';
   }
 }
